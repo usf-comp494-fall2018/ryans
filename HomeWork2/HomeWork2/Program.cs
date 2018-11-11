a049c51dea3975c2e9f16f197852a184b7136d3e
Ryan Schuda <RyanSchuda@stfrancis.edu>
2018-11-11 17:51:16 -0600

    Added HomeWork2

---
+++ HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Tom", "Bob", "Dana", "Julie", "Sarah", "Fred", "Waldo", "Jenny", "Cathy" };
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i] == "Waldo")
+                {
+                    Console.WriteLine("I found Waldo! (Position # " + i + ")");
+                }
+            }
+        }
+    }
+}
